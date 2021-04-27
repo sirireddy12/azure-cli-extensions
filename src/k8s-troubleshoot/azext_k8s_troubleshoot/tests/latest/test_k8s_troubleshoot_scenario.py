81c24ffcf133759325b42309d7b39b73071ed1b3
Arpit Gupta <arpgu@microsoft.com>
2021-04-27 13:46:32 +0530

    fix

---
+++ src/k8s-troubleshoot/azext_k8s_troubleshoot/tests/latest/test_k8s_troubleshoot_scenario.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import os
+import unittest
+
+from azure_devtools.scenario_tests import AllowLargeResponse
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
+
+
+TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
+
+
+class K8s_troubleshootScenarioTest(ScenarioTest):
+
+    @ResourceGroupPreparer(name_prefix='cli_test_k8s_troubleshoot')
+    def test_k8s_troubleshoot(self, resource_group):
+        pass
